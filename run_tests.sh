24f7a7ba62f4aae3cbe771020131dad7bdf37033
plilja <patlil@kth.se>
2017-02-12 16:39:31 +0100

    Add simple test for include files

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-for t in ./test/*.vr; do
-    ./bin/varan $t -o .test
+function run_single_test {
+    ./bin/varan ${@} -o .test
     ./.test > /dev/null
     rm .test 
+}
+
+# Single files directly in test folder
+for t in ./test/*.vr; do
+    run_single_test $t
 done
 
+# Subdirectories consisting of multiple source files
+for t in ./test/*/; do
+    run_single_test `find $t/*.vr`
+done
