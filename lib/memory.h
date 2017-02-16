ede3cbbd9db8919415dad06521ecf38696ca7252
plilja <patlil@kth.se>
2017-02-16 19:02:27 +0100

    Only expose methods that are used from outside in memory.h

--- lib/memory.h
+++ lib/memory.h
@@ -14,9 +14,6 @@ void add_type(struct Type t);
 void init();
 void tear_down();
 
-int copy(void *dest, void *src);
-void garbage_collect();
-
 void* alloc(size_t size);
 void* stack_push(void *p);
 void stack_reset(void *p);
